4e69a4328ab6bf31cc39a5aac360687d1c74b7e8
Tiến <letien3413@gmail.com>
2024-01-16 10:16:40 +0700

    Update Demo.java
    Tính điểm trung bình

--- BaiTapDeMo/src/tienlv/Demo.java
+++ BaiTapDeMo/src/tienlv/Demo.java
@@ -1,12 +1,28 @@
 package tienlv;
 
+import java.util.Calendar;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5, b = 6, c;
-		c = a + b;
-		System.out.print(c);
+		// cộng 2 số nguyên
+//		int a = 5, b = 6, c;
+//		c = a + b;
+//		System.out.print(c);
+		
+		// tính điểm trung bình
+		float a = 8.7f;
+		float b = 7.2f;
+		float c = 6.0f;
+		float dtb = (a * 2 + b + c)/4;
+		System.out.printf("Điểm trung bình: %.3f", dtb);
+		
+		// khai báo 2 biến họ tên và năm sinh, tính tuổi
+		String hoTen = "Lê Văn Tiến";
+		int namSinh = 2003;
+		int age;
+		Calendar ca = Calendar
 	}
 
 }
