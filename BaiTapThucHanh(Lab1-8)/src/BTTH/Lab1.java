75496191ac7b71562a7ccaae770e9da5f63aedd2
Tiến <letien3413@gmail.com>
2024-01-16 11:35:27 +0700

    Cập nhật BT Thực hành ( Lab 1- 8)
    Cập nhật Lab1 ( Bài 2)

--- BaiTapThucHanh(Lab1-8)/src/BTTH/Lab1.java
+++ BaiTapThucHanh(Lab1-8)/src/BTTH/Lab1.java
@@ -4,26 +4,50 @@ import java.util.Scanner;
 
 public class Lab1 {
 
-	public static void main(String[] args) {
-		// Bài 1
-		Scanner s = new Scanner(System.in);
-		String hoTen;
-		float dtb;
-		
-		System.out.print("Nhập họ tên: ");
-		hoTen = s.nextLine();
-		
-		System.out.print("Nhập điểm trung bình: ");
-		dtb = s.nextFloat();
-		
-		System.out.print(hoTen + " " + dtb + " điểm\n");
-		System.out.println(hoTen + " " + dtb + " điểm");
-		System.out.printf("%s %.2f điểm", hoTen, dtb);
-		s.close();
-		
-		
-		// Bài 2
-		
-	}
+    public static void main(String[] args) {
+        //bai1();
+        bai2();
+    }
 
+    //  Bài 1
+    public static void bai1() {
+        Scanner s = new Scanner(System.in);
+        String hoTen;
+        float dtb;
+
+        System.out.print("Nhập họ tên: ");
+        hoTen = s.nextLine();
+
+        System.out.print("Nhập điểm trung bình: ");
+        dtb = s.nextFloat();
+
+        System.out.print(hoTen + " " + dtb + " điểm\n");
+        System.out.println(hoTen + " " + dtb + " điểm");
+        System.out.printf("%s %.2f điểm", hoTen, dtb);
+        s.close();
+    }
+
+    // Bài 2
+    public static void bai2() {
+        Scanner s = new Scanner(System.in);
+        int chieuDai, chieuRong;
+        System.out.print("Nhập chiều dài: ");
+        chieuDai = s.nextInt();
+
+        do {
+            System.out.print("Nhập chiều rộng: ");
+            chieuRong = s.nextInt();
+            if (chieuRong >= chieuDai) {
+                System.out.print("Chiều rộng phải bé hơn chiều dài\n");
+            }
+        } while (chieuRong >= chieuDai);
+
+        int chuVi = (chieuDai + chieuRong) * 2;
+        int dienTich = chieuDai * chieuRong;
+        int canhMin = Math.min(chieuDai, chieuRong);
+        System.out.print("Chu vi: " + chuVi);
+        System.out.println("\nDiện tích: " + dienTich);
+        System.out.printf("Cạnh nhỏ nhất: %d", canhMin);
+        s.close();
+    }
 }
