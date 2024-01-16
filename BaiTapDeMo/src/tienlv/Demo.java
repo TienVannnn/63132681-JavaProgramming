459d6051114328cee7f8d2a6cf684fe255c2f311
Tiến <letien3413@gmail.com>
2024-01-16 10:04:57 +0700

    Bài tập demo cơ bản trong slide
    cộng 2 số nguyên

---
+++ BaiTapDeMo/src/tienlv/Demo.java
@@ -0,0 +1,12 @@
+package tienlv;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 6, c;
+		c = a + b;
+		System.out.print(c);
+	}
+
+}
