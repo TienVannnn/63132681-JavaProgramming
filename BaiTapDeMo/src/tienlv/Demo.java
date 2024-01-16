a2f42857f6f658c01ffd5d01202d4595d5fd6f8e
Tiến <letien3413@gmail.com>
2024-01-16 10:25:07 +0700

    Update Demo.java
    Khai báo hoten và năm sinh, in ra màn hình

--- BaiTapDeMo/src/tienlv/Demo.java
+++ BaiTapDeMo/src/tienlv/Demo.java
@@ -16,13 +16,16 @@ public class Demo {
 		float b = 7.2f;
 		float c = 6.0f;
 		float dtb = (a * 2 + b + c)/4;
-		System.out.printf("Điểm trung bình: %.3f", dtb);
+		System.out.printf("Điểm trung bình: %.3f\n", dtb);
 		
-		// khai báo 2 biến họ tên và năm sinh, tính tuổi
+		// khai báo 2 biến họtên và năm sinh, tính tuổi và in ra màn hình
 		String hoTen = "Lê Văn Tiến";
 		int namSinh = 2003;
 		int age;
-		Calendar ca = Calendar
+		Calendar ca = Calendar.getInstance();
+		age = ca.get(Calendar.YEAR) - namSinh;
+		System.out.print(hoTen + " năm nay " + age + " tuổi");
+		System.out.printf("\n%s năm nay %d tuổi", hoTen, age);
 	}
 
 }
